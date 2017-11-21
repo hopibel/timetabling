95fca8a9158583caa8e9cdb002716516ab6e9290
Hope Ibelgaufts <hopibel@gmail.com>
2017-11-21 21:13:30 +0800

    Added rooms, faster overlap calculation, and bounds checking on slots

--- timetabling.py
+++ timetabling.py
@@ -1,26 +1,38 @@
 """Timetabling using a genetic algorithm"""
 
 import random
-import itertools
 import numpy
 from deap import algorithms, base, creator, tools
 
 SLOTS = 90
 
 
-def gen_ind(courses):
+def gen_ind(courses, rooms):
     """Generate individual."""
     ind = []
     for course in courses:
+        length = course['length']
+        room = random.choice(rooms)
         if course['split'] and random.random() < 80:  # 80% of classes are twice a week
-            slot = [random.randrange(SLOTS) for i in range(2)]
+            ind.append([
+                {
+                    'slot': random.randrange(SLOTS - length // 2),
+                    'len': length // 2,
+                    'room': room,
+                } for _ in range(2)
+            ])
         else:
-            slot = [random.randrange(SLOTS)]
-        ind.append(slot)
+            ind.append([
+                {
+                    'slot': random.randrange(SLOTS - length),
+                    'len': length,
+                    'room': room,
+                },
+            ])
     return ind
 
 
-def eval_timetable(individual, courses):
+def eval_timetable(individual):
     """Calculate timetable cost.
 
     Currently returns:
@@ -28,50 +40,73 @@ def eval_timetable(individual, courses):
     """
     overlap = 0
 
-    for i, course in enumerate(individual):
-        length = courses[i]['length']
-        sessions = len(course)
-        lps = int(length / sessions)
+    # O(n**2). average time complexity is probably around O(nlogn)
+    times = []
+    for course in individual:
+        for session in course:
+            times.append(session)
+    times.sort(key=lambda x: x['slot'])
 
-        if length % sessions != 0:
-            raise ValueError(
-                "Course {} with length {} cannot be divided into {} sessions per week".format(
-                    i, length, sessions))
-
-        # self-overlap
-        for a, b in itertools.combinations(course, 2):
-            width = max(a, b)+lps - min(a, b)
-            if width < 2*lps:
-                overlap += 2*lps - width
-
-        # overlap
-        for j in range(i+1, len(individual)):
-            for a, b in itertools.product(course, individual[j]):
-                blps = int(courses[j]['length'] / len(individual[j]))
-                width = max(a+lps, b+blps) - min(a, b)
-                if width < lps + blps:
-                    overlap += lps + blps - width
+    for i, _ in enumerate(times):
+        a = times[i]
+        for j in range(i + 1, len(times)):
+            b = times[j]
+            if a['room'] != b['room']:
+                continue
+            if b['slot'] >= a['slot'] + a['len']:  # b and everything after don't overlap with a
+                break
+            width = max(a['slot']+a['len'], b['slot']+b['len']) - min(a['slot'], b['slot'])
+            if width < a['len'] + b['len']:
+                overlap += a['len'] + b['len'] - width
 
     return (overlap,)
 
 
-def mut_timetable(individual, courses):
-    """Move a class up or down a single time slot."""
-    course = random.choice(range(len(individual)))
-    session = random.randrange(len(individual(course)))
-    course[session] += random.choice((1, -1))
-    # TODO: bounds checking
+def mut_timetable(individual, rooms):
+    """Mutate a timetable.
+
+    Shift a class timeslot by 1
+    Change classrooms
+    """
+    course = random.choice(individual)
+    session = random.choice(course)
+
+    x = random.random()
+    if x < 0.5:
+        # shift timeslot
+        session['slot'] += random.choice((1, -1))
+    else:
+        # change room
+        session['room'] = random.choice(rooms)
+
+    # TODO: change split to single or vice versa
+
+    # bounds checking
+    # if moving one way goes out of bounds, move the other way
+    if session['slot'] < 0:
+        session['slot'] = 1
+    elif session['slot'] + session['len'] >= SLOTS:
+        session['slot'] = SLOTS - session['len'] - 1
+
+    # day boundary checking
+    # randomly put it on one side of the boundary
+    start = session['slot']
+    end = start + session['len'] - 1
+    if start // 18 != end // 18:
+        bound = int(end / 18) * 18
+        session['slot'] = random.choice((bound - session['len'], bound))
+
     return (individual,)
 
 
 def main():
     """Entry point if called as executable."""
 
-    random.seed(64)
+    random.seed('feffy')
 
     # dummy course table
     courses = []
-    for name in 'abcdefghij':
+    for name in 'abcdefghijklmnopqr':
         for sec in range(1, 3):
             course = {
                 'name': name,
@@ -83,23 +118,28 @@ def main():
                 course['split'] = 0
             courses.append(course)
 
+    # dummy room table
+    rooms = []
+    for i in range(3):
+        rooms.append(i)
+
     creator.create("Fitness", base.Fitness, weights=(-1.0,))
     creator.create("Individual", list, fitness=creator.Fitness)
 
     toolbox = base.Toolbox()
-    toolbox.register("ind", gen_ind, courses=courses)
+    toolbox.register("ind", gen_ind, courses=courses, rooms=rooms)
     toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ind)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-    toolbox.register("evaluate", eval_timetable, courses=courses)
-    toolbox.register("mate", tools.cxUniform, indpb=0.1)
-    toolbox.register("mutate", mut_timetable, courses=courses)
+    toolbox.register("evaluate", eval_timetable)
+    toolbox.register("mate", tools.cxOnePoint)
+    toolbox.register("mutate", mut_timetable, rooms=rooms)
     toolbox.register("select", tools.selTournament, tournsize=2)
 
-    gens = 1000  # generations
+    gens = 100  # generations
     mu = 1000  # population size
     lambd = 1000  # offspring to create
-    cxpb = 0.7  # crossover probability
+    cxpb = 0.8  # crossover probability
     mutpb = 0.2  # mutation probability
 
     pop = toolbox.population(n=mu)
